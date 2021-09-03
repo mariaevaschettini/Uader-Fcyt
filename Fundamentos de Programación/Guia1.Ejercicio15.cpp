e0faee4f81dfdae40268b10d04d64fdb8f978a39
María Eva <71808714+mariaevaschettini@users.noreply.github.com>
2021-09-03 17:23:16 -0300

    Add files via upload

---
+++ Fundamentos de Programación/Guia1.Ejercicio15.cpp
@@ -0,0 +1,29 @@
+/* Se desea obtener la cuota a pagar por un crédito solicitado. Se ingresa la información de:
+Capital solicitado, Razón (tasa anual), plazo (en meses).
+El cálculo de interés simple se realiza con la fórmula: (Capital x razón x tiempo) / (100 x 12).
+El monto que se obtenga (capital + interés) dividido el plazo da como resultado el valor de la
+cuota. */
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	float Cap; // monto del capital
+	float Ra; //tasa anual
+	float Pla; //plazo
+	float Int; //interes simple
+	float Cta; //cuota
+	cout<<"Ingresar el monto del capital ";
+	cin>>Cap;
+	cout<<"Ingresar la tasa anual ";
+	cin>>Ra;
+	cout<<"Ingresar la cantidad de meses ";
+	cin>>Pla;
+	Int = (Cap * Ra * Pla)/(100 * 12);
+	Cta = (Cap + Int)/Pla;
+	cout<<"su cuota es de "<<Cta<<endl;
+	return 0;
+}
+
---
+++ Fundamentos de Programación/Guia1.Ejercicio9.cpp
@@ -0,0 +1,26 @@
+
+/*Se desea conocer el valor del costo de un producto. Se conoce: el precio de 
+venta y su porcentaje de ganancia.*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	float V; //precio de venta del producto
+	float PorGan; //porcentaje de ganacia
+	float Cost; //costo
+	float Aux; //ganancia
+	cout<<"Ingrese en precio de venta del producto ";
+	cin>>V;
+	cout<<"Ingrese el porcentaje de ganancia ";
+	cin>>PorGan;
+	Aux = V*PorGan/100; 
+	Cost = V - Aux; 
+	cout<<"El costo del producto de $"<<Cost;
+	
+	return 0;
+}
+
