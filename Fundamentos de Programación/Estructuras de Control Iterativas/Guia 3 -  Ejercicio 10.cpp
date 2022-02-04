824526b67e68b5d8960217fda26101b23a643347
María Eva <71808714+mariaevaschettini@users.noreply.github.com>
2022-02-04 14:45:58 -0300

    Add files via upload

---
+++ Fundamentos de Programación/Estructuras de Control Iterativas/Guia 3 -  Ejercicio 10.cpp
@@ -0,0 +1,38 @@
+/*
+
+Realice un algoritmo que solicite el ingreso de nombres. Finalizar el ingreso cuando se
+ingrese un nombre igual al último ingresado. Informar cuantos nombres se ingresaron.
+
+*/
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	string nombre, nombre_ingresado;
+	int cont;
+	
+	cout<<"Ingrese un nombre: ";
+	getline (cin, nombre_ingresado);
+	cout<<"Ingrese un nombre: ";
+	getline (cin, nombre);
+	cont = 2;
+	while (nombre != nombre_ingresado){
+		cout<<"Ingrese un nombre: ";
+		getline (cin, nombre_ingresado);
+		cout<<"Ingrese un nombre: ";
+		getline (cin, nombre);
+		if (nombre != nombre_ingresado){
+			cont = cont + 2;
+		}else{
+			cont++;
+		}
+	}	
+	
+	cout<<"Se ingresaron "<<cont<<" nombres";
+		
+	
+	return 0;
+}
+
