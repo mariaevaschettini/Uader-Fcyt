8ef2f5460a96313f367fcfc98a3f84ca449e92e5
María Eva <71808714+mariaevaschettini@users.noreply.github.com>
2022-02-05 09:36:04 -0300

    Add files via upload

---
+++ Fundamentos de Programación/Estructuras de Control Iterativas/Guia 3 - Ejercicio 11.cpp
@@ -0,0 +1,28 @@
+/*
+Realice un algoritmo que solicite el ingreso de dos números positivos y muestre por
+pantalla su producto. No es posible utilizar el operador de multiplicación (*) directamente.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int numero1, numero2, cont, resultado;
+	cout<<"Ingrese un numero: ";
+	cin>>numero1;
+	cout<<"Ingrese un numero: ";
+	cin>>numero2;
+	
+	cont = 0;
+	resultado = 0;
+	while (cont<numero2){
+		cont++;
+		resultado = resultado + numero1;
+	}
+	
+	cout<<numero1<<" X "<<numero2<<" = "<<resultado;
+	
+	return 0;
+}
+
---
+++ Fundamentos de Programación/Estructuras de Control Iterativas/Guia 3 - Ejercicio 13.cpp
@@ -0,0 +1,30 @@
+/*
+Calcular la media de X cantidad de números, para lo cual se ingresarán valores enteros
+reiteradamente, siendo el fin de la carga, un valor igual a 0. Informar el valor medio y la
+cantidad de datos ingresados.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int cont, media, acumulador, numero;
+	
+	cout<<"Ingrese un valor: ";
+	cin>>numero;
+	cont = 0;
+	acumulador = 0;
+	
+	while (numero !=0){
+		acumulador = acumulador + numero;
+		cont++;
+		cout<<"Ingrese un valor: ";
+		cin>>numero;
+	}
+	media = acumulador / cont;
+	cout<<"La media es de: "<<media<<endl;
+	cout<<"La cantidad de datos ingresados es de: "<<cont;
+	
+	return 0;
+}
+
