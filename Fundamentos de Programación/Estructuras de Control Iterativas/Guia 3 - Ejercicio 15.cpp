702a0d222d451115b003cc4168179cd1fd8ce5d5
María Eva <71808714+mariaevaschettini@users.noreply.github.com>
2022-02-06 09:09:52 -0300

    Add files via upload

---
+++ Fundamentos de Programación/Estructuras de Control Iterativas/Guia 3 - Ejercicio 15.cpp
@@ -0,0 +1,38 @@
+/*
+Generar números al azar entre 0 y 999. Detener la generación de números cuando se
+haya generado 2 veces consecutivas un número ingresado. Informar la cantidad de números
+que fueron generados al momento de la detención.
+*/
+
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int cont, numero, acumulador, detencion_num;
+	
+	cout<<"Ingrese el numero para detener la ejecucion de numeros aleatorios: ";
+	cin>>detencion_num;
+	cout<<endl;
+	
+	cont = 0;
+	acumulador = 0;
+	srand(time(NULL));
+	
+	do{
+		
+		numero = rand()%999 + 1;
+		cout<<numero<<endl;
+		cont++;
+		
+		if (numero == detencion_num){
+			acumulador = acumulador + 1;
+		}
+	}while (acumulador != 2);
+	
+	cout<<"Se generaron "<<cont<<" numeros"<<endl;
+	
+	return 0;
+}
+
